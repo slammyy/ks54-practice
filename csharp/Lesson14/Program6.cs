fd7be60f9cb3bda2afa023b6e38aa637bb2448fb
s1ammy <masav@Maxims-MacBook-Air.local>
2023-01-19 14:37:41 +0300

    feat: add Program6.cs

---
+++ csharp/Lesson14/Program6.cs
@@ -0,0 +1,28 @@
+namespace Program6
+{
+  struct Struct
+  {
+    public char symbol;
+    public int number;
+
+    public void Show()
+    {
+      Console.WriteLine("Symbol: {0}\nNumber: {1}", symbol, number);
+    }
+
+  }
+
+  class Program
+  {
+    unsafe static void Main()
+    {
+      Struct copy;
+      Struct* pStruct;
+      pStruct = &copy;
+
+      pStruct->symbol = 'a';
+      pStruct->number = 14;
+      pStruct->Show();
+    }
+  }
+}
